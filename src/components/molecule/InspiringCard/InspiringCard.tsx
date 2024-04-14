3fe9ae5286895b7e76bfb130cea853ff8f1ef1ee
Sachin <sachin@codewalnut.com>
2024-04-14 19:13:04 +0530

    feat: add inspring stories

--- src/components/molecule/InspiringCard/InspiringCard.tsx
+++ src/components/molecule/InspiringCard/InspiringCard.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-
+import "./InspiringCard.css";
 interface InspiringCardProps {
   imageSrc: string;
   title: string;
   description: string;
+  imageAlignment?: string;
 }
+
 const InspiringCard = ({
   imageSrc,
   title,
   description,
+  imageAlignment = "left", // Default value is "left"
 }: InspiringCardProps) => {
+  const isRightAligned = imageAlignment === "right";
+
   return (
     <div className="inspiring-card">
       <div className="row g-0">
-        <div className="col-md-4">
+        {isRightAligned && (
+          <div className="col-md-8  p-3">
+            <Card.Body className="text-end">
+              <Card.Title className="inspiring-card-title">{title}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+            </Card.Body>
+          </div>
+        )}
+        <div className="col-md-4 p-3">
           <Card.Img src={imageSrc} alt="Card image" />
         </div>
-        <div className="col-md-8">
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{description}</Card.Text>
-          </Card.Body>
-        </div>
+        {!isRightAligned && (
+          <div className="col-md-8  p-3">
+            <Card.Body>
+              <Card.Title className="inspiring-card-title">{title}</Card.Title>
+              <Card.Text>{description}</Card.Text>
+            </Card.Body>
+          </div>
+        )}
       </div>
     </div>
   );
