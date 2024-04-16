e8326bb0ae812315280a0457a2123ab37f6ec848
Sachin <sachin@codewalnut.com>
2024-04-16 09:51:59 +0530

    perf:deployment triggered

--- src/routes/allRoutes.tsx
+++ src/routes/allRoutes.tsx
@@ -1,12 +1,8 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "*", // Catch-all route
-    element: <Navigate to="/login" />, // Redirect to the login page if unauthenticated
   },
 ]);
