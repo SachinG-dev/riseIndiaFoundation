a90a71b208da3d35663894b02903415c43e6cacd
Umesh <umesh@codewalnut.com>
2024-04-16 22:35:07 +0530

    format: prettier

--- src/components/organism/Testimonials/Testimonials.tsx
+++ src/components/organism/Testimonials/Testimonials.tsx
@@ -1,49 +1,67 @@
 import React from "react";
 import Slider from "react-slick";
-import './Testimonials.css'
+import "./Testimonials.css";
 
 const testimonialData: ProfileCardProps[] = [
   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
     imageAlt: "Profile picture of Sachin Bagal",
     name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
-  },
-   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
-    imageAlt: "Profile picture of Sachin Bagal",
-    name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
   },
   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
     imageAlt: "Profile picture of Sachin Bagal",
     name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
   },
   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
     imageAlt: "Profile picture of Sachin Bagal",
     name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
   },
   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
     imageAlt: "Profile picture of Sachin Bagal",
     name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
   },
   {
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
     imageAlt: "Profile picture of Sachin Bagal",
     name: "Sachin Bagal",
-    title: "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
-    description: "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety."
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
+  },
+  {
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/bed2aba62f4a32283ec0c1e10d3f92cbaff5322e4c71f38ee444e418e199f4e6?apiKey=f2619df92a124e69bbb0f345b6e57c75&",
+    imageAlt: "Profile picture of Sachin Bagal",
+    name: "Sachin Bagal",
+    title:
+      "Manager- Business Development and Project Manager MHPDS, Integra Micro Systems (P) Ltd.",
+    description:
+      "During the Covid-19 pandemic, Rise India Foundation brought smiles to the faces of struggling daily wagers and laborers across the country by providing essential food packets. Additionally, despite budget constraints, the foundation supplied vital precautionary products like sanitizers and masks to BEST workers and Police officials, demonstrating their commitment to safety.",
   },
 ];
 
@@ -53,7 +71,13 @@ interface ProfileImageProps {
 }
 
 const ProfileImage = ({ src, alt }: ProfileImageProps) => (
-  <img loading="lazy" src={src} alt={alt} className="rounded-circle aspect-ratio-square" style={{ width: "67px" }} />
+  <img
+    loading="lazy"
+    src={src}
+    alt={alt}
+    className="rounded-circle aspect-ratio-square"
+    style={{ width: "67px" }}
+  />
 );
 
 interface ProfileNameProps {
@@ -88,7 +112,13 @@ interface ProfileCardProps {
   description: string;
 }
 
-const ProfileCard = ({ imageSrc, imageAlt, name, title, description }: ProfileCardProps) => (
+const ProfileCard = ({
+  imageSrc,
+  imageAlt,
+  name,
+  title,
+  description,
+}: ProfileCardProps) => (
   <article className="d-flex flex-column align-items-center p-5 text-center bg-white testimonial-card">
     <ProfileImage src={imageSrc} alt={imageAlt} />
     <ProfileName name={name} />
@@ -104,14 +134,14 @@ interface NextArrowProps {
 
 const NextArrow = ({ className, onClick }: NextArrowProps) => {
   return (
-    <div
-      className={className}
-      onClick={onClick}
-      >
-      <img src="http://localhost:3000/assets/images/left-arrow-indicator.svg" alt="Prev" />
+    <div className={className} onClick={onClick}>
+      <img
+        src="http://localhost:3000/assets/images/left-arrow-indicator.svg"
+        alt="Prev"
+      />
     </div>
   );
-}
+};
 
 interface PrevArrowProps {
   className?: string;
@@ -120,14 +150,14 @@ interface PrevArrowProps {
 
 const PrevArrow = ({ className, onClick }: PrevArrowProps) => {
   return (
-    <div
-      className={className}
-      onClick={onClick}
-    >
-      <img src="http://localhost:3000/assets/images/right-arrow-indicator.svg" alt="Prev" />
+    <div className={className} onClick={onClick}>
+      <img
+        src="http://localhost:3000/assets/images/right-arrow-indicator.svg"
+        alt="Prev"
+      />
     </div>
   );
-}
+};
 
 export function Testimonials() {
   let settings = {
@@ -147,42 +177,46 @@ export function Testimonials() {
         settings: {
           slidesToShow: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <section className="testimonial-section">
-      <h1 className="testimonial-title text-center mb-5"><span className="text-color">“Testimonials of</span> Success and Excellence”</h1>
+      <h1 className="testimonial-title text-center mb-5">
+        <span className="text-color">“Testimonials of</span> Success and
+        Excellence”
+      </h1>
       <div className="slider-container">
         <Slider {...settings}>
-        {!!testimonialData && testimonialData.map((profile: ProfileCardProps, key: number) => (
-            <ProfileCard
-              key={key}
-              imageSrc={profile.imageSrc}
-              imageAlt={profile.imageAlt}
-              name={profile.name}
-              title={profile.title}
-              description={profile.description}
-            />
-          ))}
+          {!!testimonialData &&
+            testimonialData.map((profile: ProfileCardProps, key: number) => (
+              <ProfileCard
+                key={key}
+                imageSrc={profile.imageSrc}
+                imageAlt={profile.imageAlt}
+                name={profile.name}
+                title={profile.title}
+                description={profile.description}
+              />
+            ))}
         </Slider>
       </div>
     </section>
