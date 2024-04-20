819c1a68c013c99f11b23120e2384594f932d2b9
vishwakarma <vishwakarmacodewalnut@gmail.com>
2024-04-20 18:26:02 +0530

    feature:footer and hero section added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import Banner from "../components/organism/Banner/Banner";
+import HeroBanner from "../components/organism/HeroBanner/HeroBanner";
 import { AboutRiseIndiaFoundation } from "../components/organism/AboutRiseIndiaFoundation/AboutRiseIndiaFoundation";
 import { ImpactSection } from "../components/organism/ImpactSection/ImpactSection";
 import { Testimonials } from "../components/organism/Testimonials/Testimonials";
@@ -9,7 +9,7 @@ import { Layout } from "../components/layout/";
 export function Home() {
   return (
     <Layout>
-      <Banner />
+      <HeroBanner />
       <AboutRiseIndiaFoundation />
       <ImpactSection />
       <Testimonials />
