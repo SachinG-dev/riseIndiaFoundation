916b58b8897f8e8345b7281fcc4de4a02f1cf3cb
vishwakarma <vishwakarmacodewalnut@gmail.com>
2024-04-20 18:26:02 +0530

    feature:footer and hero section added

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,11 +1,89 @@
+import { Col, Row } from "react-bootstrap";
+import "./Footer.css";
+import Button from "../../atom/Buttons/Button";
+
 export function Footer() {
+  const onClickFunction = () => {
+    const onclickUrl = "https://www.google.com";
+    window.location.href = onclickUrl;
+  };
   return (
-    <footer className="footer py-3">
-      <div className="container-fluid d-flex justify-content-center">
-        <span className="text-dark d-block text-center text-sm-start d-sm-inline-block">
-          © MD Tel. All Rights Reserved
-        </span>
+    <footer className="w-auto">
+      <div className=" row d-flex flex-column flex-md-row justify-content-center gutter-unset">
+        <Button onClick={onClickFunction} className="col footer-button">
+          Become a Volunteer
+        </Button>
+        <Button onClick={onClickFunction} className="col footer-button">
+          Make a Donation
+        </Button>
+        <Button onClick={onClickFunction} className="col footer-button">
+          Support a Campaign
+        </Button>
       </div>
+      <section className="d-flex flex-column justify-content-center bg footer-padding">
+        <Row>
+          <Col>
+            <img
+              src="/assets/images/rise-india-logo.png"
+              alt="Rise-India-Logo"
+            />
+          </Col>
+          <Col className="footer-heading">
+            <div>Support</div>
+            <div className="footer-text d-flex flex-column">
+              <span>Contact us</span>
+              <span>FAQs</span>
+              <span>Terms & Conditions</span>
+              <span>Policies</span>
+            </div>
+          </Col>
+          <Col className="footer-heading">
+            <div>Rise India Foundation</div>
+            <div className="footer-text d-flex flex-column gap-1">
+              <span>About us</span>
+              <span>Our Work</span>
+              <span className="text-decoration-underline">
+                change@riseindiafoundation.in
+              </span>
+              <span className="text-decoration-underline">
+                donation@riseindiafoundation.in
+              </span>
+            </div>
+          </Col>
+          <Col className="footer-heading">
+            <div>Join us</div>
+            <div className="footer-text d-flex flex-column">
+              <span>Become a volunteer</span>
+              <span>Make a donation</span>
+              <span>Support a Campaign</span>
+            </div>
+          </Col>
+        </Row>
+        <div className="footer-divider"></div>
+        <Row>
+          <div className="d-flex justify-content-center flex-wrap gap-5 p-0">
+            <a href="#">
+              <img src="./assets/facebook-icon.svg" alt="Facebook-Icon" />
+            </a>
+
+            <a href="#">
+              <img src="./assets/instagram-icon.svg" alt="Instagram-Icon" />
+            </a>
+
+            <a href="#">
+              <img src="./assets/linkedin-icon.svg" alt="Linkedin-Icon" />
+            </a>
+
+            <a href="#">
+              <img src="./assets/tumblr-icon.svg" alt="Tumblr-Icon" />
+            </a>
+
+            <a href="#">
+              <img src="./assets/twitter-icon.svg" alt="Twitter-Icon" />
+            </a>
+          </div>
+        </Row>
+      </section>
     </footer>
   );
 }
