6d88349f61cefa7c0b5c7d9c84500c779b282def
Sachin <sachin@codewalnut.com>
2024-04-20 16:55:14 +0530

    about us page added

--- src/routes/allRoutes.tsx
+++ src/routes/allRoutes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { ContactUs } from "../pages/ContactUs";
+import { AboutUs } from "../pages/AboutUs";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -9,5 +10,9 @@ export const router = createBrowserRouter([
   {
     path: "/contact-us",
     element: <ContactUs />,
+  },
+  {
+    path: "/about-us",
+    element: <AboutUs />,
   },
 ]);
