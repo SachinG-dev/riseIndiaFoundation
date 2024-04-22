0cf531216411f0f359791a95f0c9f600062d0b9b
Sreenath Chadive <sreenath@codewalnut.com>
2024-04-22 19:43:12 +0530

    fix: updated icons for program cards

--- src/components/organism/WorkInitiatives/ProgramInitiatives.tsx
+++ src/components/organism/WorkInitiatives/ProgramInitiatives.tsx
@@ -1,45 +1,39 @@
 import React from 'react';
 import './ProgramInitiatives.css';
-import { Container } from 'react-bootstrap';
 import ProgramCard from '../../../components/atom/ProgramCard/ProgramCard';
 
 export function ProgramInitiatives() {
   const programCardsData = [
     {
-      iconSrc:
-        'https://www.raiseindiafoundation.org/wp-content/uploads/2022/10/raise-logo.png',
+      iconSrc: '/assets/svg/education-logo.svg',
       programName: 'Education',
       programDescription:
         'Illuminating Minds, Empowering Futures. Our education initiatives ensure access to quality education for underprivileged children, equipping them with knowledge and skills to break the cycle of poverty.',
       btnLink: '#',
     },
     {
-      iconSrc:
-        'https://www.raiseindiafoundation.org/wp-content/uploads/2022/10/raise-logo.png',
+      iconSrc: '/assets/svg/health-logo.svg',
       programName: 'Health',
       programDescription:
         'Nurturing Wellness, Saving Lives. Our healthcare programs provide essential medical services, promote preventive care, and raise awareness to improve the well-being of communities in need.',
       btnLink: '#',
     },
     {
-      iconSrc:
-        'https://www.raiseindiafoundation.org/wp-content/uploads/2022/10/raise-logo.png',
+      iconSrc: '/assets/svg/disaster-relief.svg',
       programName: 'Disaster Relief',
       programDescription:
         'Rebuilding Lives, Restoring Hope. We swiftly respond to natural disasters, offering immediate relief, shelter, and support to affected individuals and communities, aiding in their recovery and resilience.',
       btnLink: '#',
     },
     {
-      iconSrc:
-        'https://www.raiseindiafoundation.org/wp-content/uploads/2022/10/raise-logo.png',
+      iconSrc: '/assets/svg/advocacy-logo.svg',
       programName: 'Advocacy',
       programDescription:
         'Amplifying Voices, Driving Change. Through advocacy efforts, we strive to bring attention to social issues, drive policy changes, and create a platform for marginalized communities to be heard.',
       btnLink: '#',
     },
     {
-      iconSrc:
-        'https://www.raiseindiafoundation.org/wp-content/uploads/2022/10/raise-logo.png',
+      iconSrc: '/assets/svg/skill-development-logo.svg',
       programName: 'Skill Development',
       programDescription:
         'Unlocking Potential, Opening Doors. Our skill development programs provide vocational training and mentorship, empowering individuals with marketable skills to enhance employability and achieve self-sufficiency.',
@@ -49,9 +43,9 @@ export function ProgramInitiatives() {
 
   return (
     <section className='program-initiatives-section py-5'>
-      <Container>
-        <div className='py-5'>
-          <h2 className='section-title text-lg-start text-center'>
+      <div className='container'>
+        <div className='pt-5'>
+          <h2 className='jumbotron-heading text-lg-start text-center'>
             <span className='font-color-orange'>Our Work&nbsp;</span>
             <span className='font-dark-gray'>Initiatives and Programs</span>
           </h2>
@@ -68,7 +62,7 @@ export function ProgramInitiatives() {
             />
           ))}
         </div>
-      </Container>
+      </div>
     </section>
   );
 }
