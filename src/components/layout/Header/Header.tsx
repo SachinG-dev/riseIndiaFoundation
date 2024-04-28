2e554dd167e0ce79f573ad4681c47652f1f5713c
Sachin <sachin@codewalnut.com>
2024-04-28 10:23:41 +0530

    adidng our work page'

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,25 +1,24 @@
 import { Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header className="w-100 position-fixed z-100 shadow-sm bg-light">
       <Navbar bg="light" expand="lg">
         <div className="container">
-          <img
-            src="/assets/images/rise-india-logo.png"
-            alt="rise-india-foundation-logo"
-            style={{ height: "6vh" }}
-          />
+          <Link to="/">
+            <img
+              src="/assets/images/rise-india-logo.png"
+              alt="rise-india-foundation-logo"
+              style={{ height: "6vh" }}
+            />
+          </Link>
 
           <Navbar.Toggle aria-controls="navbarNav" />
           <Navbar.Collapse id="navbarNav" className="justify-content-end gap-4">
             <Nav>
               <Nav.Link href="/about-us">About us</Nav.Link>
-              <NavDropdown title="Our Work" id="services-dropdown">
-                <NavDropdown.Item href="#">Service 1</NavDropdown.Item>
-                <NavDropdown.Item href="#">Service 2</NavDropdown.Item>
-                <NavDropdown.Item href="#">Service 3</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/our-work">Our Work</Nav.Link>
               <NavDropdown title="Get Involved" id="services-dropdown">
                 <NavDropdown.Item href="/donation-and-tax-benefits">
                   Donation & Tax Benefits
