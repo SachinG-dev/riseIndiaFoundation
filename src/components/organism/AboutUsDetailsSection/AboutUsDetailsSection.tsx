bc17f13558339616e8422f8854055f8fc76668f2
Sreenath Chadive <sreenath@codewalnut.com>
2024-05-02 00:21:31 +0530

    fix: resolve comments on pr

--- src/components/organism/AboutUsDetailsSection/AboutUsDetailsSection.tsx
+++ src/components/organism/AboutUsDetailsSection/AboutUsDetailsSection.tsx
@@ -3,30 +3,30 @@ import { AboutUsCustomerCard } from '../../../components/atom/AboutUsCustomerCar
 export function AboutUsDetailsSection() {
   const aboutRiseIndiaKeyFeature = [
     {
-      backgroundColor: 'bg-light-gray',
+      backgroundColor: 'grey',
       heading: 'Empowerment',
-      headingColor: 'font-color-orange',
+      headingColor: 'orange',
       description:
         'Empowering individuals and communities by providing tools, knowledge, and resources to take charge of their own destinies, foster self-reliance and sustainable growth. We aim to, through our programs and initiatives, create a platform where every person can rise above challenges, realize their potential, actively contributing to the betterment of society.',
     },
     {
-      backgroundColor: 'bg-white',
+      backgroundColor: 'white',
       heading: 'Transparency',
-      headingColor: 'font-dark-gray',
+      headingColor: 'gray',
       description:
         'Transparency being the core of our operations, we pledge to uphold the highest standards of integrity, accountability, and openness in our interactions with stakeholders, partners, and beneficiaries. We are committed to providing clear, accurate, timely information about our activities, impact, and financials, ensuring that our stakeholders have full confidence in us.',
     },
     {
-      backgroundColor: 'bg-light-gray',
+      backgroundColor: 'grey',
       heading: 'Inclusivity',
-      headingColor: 'font-color-orange',
+      headingColor: 'orange',
       description:
         'Dedicated to creating long-lasting, sustainable solutions that address the root of social issues, striving to leave a positive, enduring impact on the communities we serve. Adopting environmentally conscious practices and promoting responsible resource management, to contribute to a more sustainable and resilient future for both people and the planet.',
     },
     {
-      backgroundColor: 'bg-white',
+      backgroundColor: 'white',
       heading: 'Sustainability',
-      headingColor: 'font-dark-gray',
+      headingColor: 'gray',
       description:
         'Dedicated to creating long-lasting, sustainable solutions that address the root of social issues, striving to leave a positive, enduring impact on the communities we serve. Adopting environmentally conscious practices and promoting responsible resource management, to contribute to a more sustainable and resilient future for both people and the planet.',
     },
@@ -55,10 +55,10 @@ export function AboutUsDetailsSection() {
     },
   ];
 
-  const aboutUsParagraph = () => {
-    return (
-      <section>
-        <div className='container py-5'>
+  return (
+    <>
+      <section className='py-5'>
+        <div className='container'>
           <h2 className='text-center fw-bold pb-4 jumbotron-heading'>
             <span className='font-color-orange'>About</span> Rise India
             Foundation
@@ -76,24 +76,17 @@ export function AboutUsDetailsSection() {
           </p>
         </div>
       </section>
-    );
-  };
-  const displayInfo = () => {
-    return aboutRiseIndiaKeyFeature.map((keyFeature, index) => {
-      return (
-        <AboutUsCustomerCard
-          key={index}
-          heading={keyFeature.heading}
-          description={keyFeature.description}
-          headingColor={keyFeature.headingColor}
-          backgroundColor={keyFeature.backgroundColor}
-        />
-      );
-    });
-  };
-
-  const OurCommitment = () => {
-    return (
+      {aboutRiseIndiaKeyFeature.map((keyFeature) => {
+        return (
+          <AboutUsCustomerCard
+            key={keyFeature.heading}
+            heading={keyFeature.heading}
+            description={keyFeature.description}
+            headingColor={keyFeature.headingColor}
+            backgroundColor={keyFeature.backgroundColor}
+          />
+        );
+      })}
       <section className='bg-light-gray'>
         <div className='container py-5'>
           <h2 className='fw-bold pt-4 jumbotron-heading'>
@@ -115,14 +108,6 @@ export function AboutUsDetailsSection() {
           </div>
         </div>
       </section>
-    );
-  };
-
-  return (
-    <>
-      {aboutUsParagraph()}
-      {displayInfo()}
-      {OurCommitment()}
     </>
   );
 }
