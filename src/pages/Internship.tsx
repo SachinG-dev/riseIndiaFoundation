820542c876a777fd13f47300625353a901529dd0
Lovepreet <lovepreet@codewalnut.com>
2024-05-13 02:00:52 +0530

    fix(codebase): codebase cleanup

--- src/pages/Internship.tsx
+++ src/pages/Internship.tsx
@@ -1,24 +1,20 @@
 import { HeroBanner } from "../components/organism/HeroBanner/HeroBanner";
 
 import { Layout } from "../components/layout";
-import { VolunteerInfoSection } from "../components/molecule/VolunteerInfoSection/VolunteerInfoSection";
 import { JoinUsForm } from "../components/molecule/JoinUsForm/JoinUsForm";
 import { JoinUsCarousels } from "../components/organism/JoinUsCarousels/JoinUsCarousels";
 import pageData from "../websiteData/internshipPageData.json";
+import { RichEditor } from "../components/atom/RichEditor/RichEditor";
 
 export function Internship() {
   const heroSectionData = pageData.heroSection;
+  const volunteerData = pageData.VolunteerSectionData;
+
 
   return (
-    <Layout footerCta={true}>
-      <HeroBanner
-        heading={heroSectionData.title}
-        description={heroSectionData.desc}
-        backgroundImageUrl={heroSectionData.backgroundImageUrl}
-        buttonText={heroSectionData.buttonText}
-        buttonURL={heroSectionData.buttonUrl}
-      />
-      <VolunteerInfoSection />
+    <Layout>
+      <HeroBanner heading={heroSectionData.title} description={heroSectionData.desc} backgroundImageUrl={heroSectionData.backgroundImageUrl}  buttonText={heroSectionData.buttonText} buttonURL={heroSectionData.buttonUrl}/>
+      <RichEditor contentBlocks={volunteerData} className="text-center py-5" />
       <JoinUsCarousels />
       <JoinUsForm title="Internship" sheetName="Internship" />
     </Layout>
