837be3e7fe6cff21fe13d3ea2eeef291ede95c7a
Lovepreet <lovepreet@codewalnut.com>
2024-05-13 10:55:20 +0530

    fix(gallery section): optimized and responsiveness

--- src/pages/Campaign.tsx
+++ src/pages/Campaign.tsx
@@ -5,12 +5,7 @@ import pageData from "../websiteData/campaignPageData.json";
 
 export const Campaign = () => {
   const heroSectionData = pageData.heroSection;
-
-  const CampaignHeading = (
-    <h2 className="font-dark-gray">
-      <span className="font-color-green">Campaigns </span>of Rise India
-    </h2>
-  );
+  const galleryData = pageData.GalleryData;
   return (
     <Layout>
       <HeroBanner
@@ -19,34 +14,9 @@ export const Campaign = () => {
         backgroundImageUrl={heroSectionData.backgroundImageUrl}
       />
       <GallerySection
-        heading={CampaignHeading}
-        images={[
-          {
-            src: "/assets/images/disaster-relief.png",
-            alt: "Disaster-Relief",
-            style: { width: "550px", height: "420px" },
-          },
-          {
-            src: "/assets/images/education.png",
-            alt: "Education",
-            style: { width: "550px", height: "420px" },
-          },
-          {
-            src: "/assets/images/health.png",
-            alt: "Health",
-            style: { width: "550px", height: "420px" },
-          },
-          {
-            src: "/assets/images/advocacy.png",
-            alt: "Advocacy",
-            style: { width: "550px", height: "420px" },
-          },
-          {
-            src: "/assets/images/skill-development.png",
-            alt: "Skills-Development",
-            style: { width: "550px", height: "420px" },
-          },
-        ]}
+        title={galleryData.title}
+        images={galleryData.images}
+        sectionClasses={galleryData.sectionClasses}
       />
     </Layout>
   );
