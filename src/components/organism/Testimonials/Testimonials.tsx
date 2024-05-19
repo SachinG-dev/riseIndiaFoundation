2ad41d6cb3032336718146653fb6ebe33c66a219
Sachin Gupta <101799996+SachinG-dev@users.noreply.github.com>
2024-05-19 12:26:25 +0530

    moving changes to prod; (#40)
    * fix(gallery section): optimized and responsiveness (#33)
    
    * fix(testimonial section): optimize testimonial section and work initiatives section (#36)
    
    * refactor(bootstrap): modified classes fir padding and margin (#35)
    
    * adding donation link (#39)
    
    ---------
    
    Co-authored-by: lovepreetCodewalnut <101801644+lovepreetCodewalnut@users.noreply.github.com>

--- src/components/organism/Testimonials/Testimonials.tsx
+++ src/components/organism/Testimonials/Testimonials.tsx
@@ -5,17 +5,17 @@ import "./Testimonials.css";
 type TestimonialClasses = {
   sectionClasses?: string;
   sectionTitleClasses?: string;
-}
+};
 
-type TestimonialData ={
-  testimonialImageUrl :string;
+type TestimonialData = {
+  testimonialImageUrl: string;
   name: string;
   userTitle: string;
   userDesc: string;
-}
-interface TestimonialsProps{
+};
+interface TestimonialsProps {
   title: string;
-  testimonialData : TestimonialData[];
+  testimonialData: TestimonialData[];
   overAllClasses: TestimonialClasses;
 }
 
@@ -33,50 +33,64 @@ var settings = {
         slidesToShow: 2,
         slidesToScroll: 1,
         dots: true,
-      }
+      },
     },
     {
       breakpoint: 900,
       settings: {
         slidesToShow: 1.5,
         slidesToScroll: 1,
-        initialSlide: 2
-      }
+        initialSlide: 2,
+      },
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
-export const Testimonials = ({title, testimonialData, overAllClasses}: TestimonialsProps) =>{
-  return(
+export const Testimonials = ({
+  title,
+  testimonialData,
+  overAllClasses,
+}: TestimonialsProps) => {
+  return (
     <section className={`testimonials ${overAllClasses.sectionClasses}`}>
       <div className="container-lg">
-        <h3 className={overAllClasses.sectionTitleClasses} dangerouslySetInnerHTML={{ __html: title }} />
+        <h3
+          className={overAllClasses.sectionTitleClasses}
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
         <div className="row">
-        <div className="testimonials__sliders">
-          <Slider {...settings}>
-            {testimonialData.map((data, index)=>{
-              return(
-                <div className="testimonial__slider--container">
-                  <div className="testimonial__slider bg-white p-4 mx-4 text-center">
-                    <img src={data.testimonialImageUrl} alt="user image" className="mx-auto"/>
-                    <h4 className="pt-3 text-primary" dangerouslySetInnerHTML={{ __html: data.name }} />
-                    <p className="pt-2 mb-0">{data.userTitle}</p>
-                    <p className="pt-3 mb-0">{data.userDesc}</p>
+          <div className="testimonials__sliders">
+            <Slider {...settings}>
+              {testimonialData.map((data, index) => {
+                return (
+                  <div className="testimonial__slider--container">
+                    <div className="testimonial__slider bg-white p-4 mx-4 text-center">
+                      <img
+                        src={data.testimonialImageUrl}
+                        alt="user profile"
+                        className="mx-auto"
+                      />
+                      <h4
+                        className="pt-3 text-primary"
+                        dangerouslySetInnerHTML={{ __html: data.name }}
+                      />
+                      <p className="pt-2 mb-0">{data.userTitle}</p>
+                      <p className="pt-3 mb-0">{data.userDesc}</p>
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-          </Slider>
-        </div>
+                );
+              })}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
